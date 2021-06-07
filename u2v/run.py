fd9f815e59d131281330f605499bf7cd83009751
samiroid <samir@inesc-id.pt>
2021-06-07 18:57:45 -0400

    fixed issue with config files

--- u2v/run.py
+++ u2v/run.py
@@ -24,36 +24,7 @@ def cmdline_args():
                         help='reset embeddings that were already computed')
     parser.add_argument('-device', type=str, default="auto", help='device')
     
-    # parser.add_argument('-encoder_type', choices=["w2v","bert","elmo","fasttext"], default="w2v", 
-    #                     help="encoder type")
     
-    # parser.add_argument('-emb', type=str, required=True, help='path to word embeddings')    
-    # parser.add_argument('-emb_enc', type=str,  default="utf-8", 
-    #                     help='word embeddings text file encoding')    
-    # parser.add_argument('-vocab_size', type=int, help='max size of vocabulary')
-    # parser.add_argument('-min_word_freq', type=int, default=5,
-    #                     help='ignore words that occur less than min_word_freq times'
-    #                     )
-    # parser.add_argument('-min_docs_user', type=int, default=3,
-    #                     help='ignore users with less min_docs_user documents')
-    # parser.add_argument('-max_docs_user', type=int, 
-    #                     help='maximum number of documents per user (debug)')
-    # parser.add_argument('-seed', type=int, default=1234, 
-    #                     help='random number generator seed')    
-    # # parser.add_argument('-neg_samples', type=int, help='number of negative samples',
-    # #                      default=10)
-    # parser.add_argument('-epochs', type=int, default=20, help='number of training epochs')    
-    # parser.add_argument('-val_split', type=float, default=0.2, help='percentage of data reserved for validation')    
-    # parser.add_argument('-lr', type=float, default=0.01, help='learning rate')    
-    # parser.add_argument('-margin', type=int, default=1, help='hinge loss margin')    
-    # parser.add_argument('-run_id', type=str, default="", help='run id')
-    # parser.add_argument('-pretrained_model', type=str, default="bert-base-uncased", 
-    #                     help='name of pretrained transformer weights')
-    # parser.add_argument('-encoder_batchsize', type=int, default=80, 
-    #                     help='encoder batch size')
-    # parser.add_argument('-batch_size', type=int, default=128, 
-    #                     help='batch size')
-    # parser.add_argument('-window_size', type=int, default=128, help='window size')
     
     return parser.parse_args()	
 
@@ -74,27 +45,27 @@ if __name__ == "__main__" :
     output_path=f"{args.output}/{encoder_type}/"
     if not os.path.exists(os.path.dirname(output_path)):
         os.makedirs(os.path.dirname(output_path)) 
+    
 
     if encoder_type == "bert":
         encoder = BERTEncoder(pretrained_weights=conf["pretrained_weights"], 
-                              encoder_batchsize=conf["encoder_batch_size"], 
+                              encoder_batchsize=conf.get("encoder_batch_size", 128), 
                               device=args.device)
         
+    elif encoder_type == "elmo":
+        encoder = ELMoEncoder(pretrained_weights=conf["pretrained_weights"], 
+                              encoder_batchsize=conf.get("encoder_batch_size", 128), 
+                              device=args.device)
+
+    elif encoder_type == "fasttext":
+        encoder = FastTextEncoder(pretrained_weights=conf["pretrained_weights"] )
+    
     elif encoder_type == "w2v":
         encoder = W2VEncoder(inpath=args.docs, outpath=output_path, 
                             embeddings_path=conf["pretrained_weights"], 
                             emb_encoding=conf.get("pretrained_weights","utf8"),
                             min_word_freq=conf.get("min_word_freq", 1),
                             max_vocab_size=conf.get("vocab_size"))
-    
-    elif encoder_type == "elmo":
-        encoder = ELMoEncoder(device=args.device, 
-                             pretrained_weights=conf["pretrained_weights"], 
-                             encoder_batchsize=conf["encoder_batch_size"])
-    
-    elif encoder_type == "fasttext":
-        encoder = FastTextEncoder(pretrained_weights=conf["pretrained_weights"] )
-    
     else:
         raise NotImplementedError
     
@@ -119,13 +90,28 @@ if __name__ == "__main__" :
             device = torch.device(args.device)
         
         print("> train")
-        
+        #get configs
+        run_id = conf.get("run_id","auto")
+        epochs = conf.get("epochs", 5)
+        margin = conf.get("margin", 1)
+        initial_lr = conf.get("lr", 0.01)
+        batch_size = conf.get("batch_size", 1)
+        val_split = conf.get("val_split", 0.2)
+
+        if run_id == "auto":            
+            run_id = f"{epochs}_{margin}_{initial_lr}"
+        #create tensorboard log
+        logs_path = f"{args.output}/logs/{encoder_type}/{run_id}/"        
+        if not os.path.exists(os.path.dirname(logs_path)):
+            os.makedirs(os.path.dirname(logs_path)) 
+
         train_model(output_path, encoder, 
-                    run_id=conf.get("run_id", ""),
-                    batch_size=conf.get("batch_size", 1),
-                    epochs=conf.get(conf["epochs"], 5),
-                    initial_lr=conf.get("lr",0.01),
-                    margin=conf.get("margin", 1),                    
-                    validation_split=conf.get("val_split", 0.2),
-                    reset=conf.get("reset_cache", False),                    
+                    run_id=run_id,
+                    epochs=epochs,
+                    initial_lr=initial_lr,
+                    margin=margin,                    
+                    batch_size=batch_size,
+                    validation_split=val_split,
+                    logs_path=logs_path,
+                    reset=args.reset_cache,                    
                     device=device)
